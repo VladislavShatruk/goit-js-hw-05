a9046167746eff275abb42b851711f899849fb7b
VladislavShatruk <vladshatruk121@gmail.com>
2024-04-14 20:24:21 +0300

    hw-05

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,89 @@
-function checkForSpam(message) {
-    normalizedMessage = message.toLowerCase();
-    if (normalizedMessage.includes("spam") || normalizedMessage.includes("sale")) {
-        return true;
-    }
-    return false;
+console.log("task-3");
+console.log("");
+
+
+const sortByDescendingFriendCount = (users) => {
+    return users.toSorted((a, b) => b.friends.length - a.friends.length);
 }
+
+
+console.log(
+    sortByDescendingFriendCount([
+      {
+        name: "Moore Hensley",
+        friends: ["Sharron Pace"],
+        gender: "male"
+      },
+      {
+        name: "Sharlene Bush",
+        friends: ["Briana Decker", "Sharron Pace"],
+        gender: "female"
+      },
+      {
+        name: "Ross Vazquez",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        gender: "male"
+      },
+      {
+        name: "Elma Head",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        gender: "female"
+      },
+      {
+        name: "Carey Barr",
+        friends: ["Jordan Sampson", "Eddie Strong"],
+        gender: "male"
+      },
+      {
+        name: "Blackburn Dotson",
+        friends: ["Jacklyn Lucas", "Linda Chapman"],
+        gender: "male"
+      },
+      {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        gender: "female"
+      }
+    ])
+  );
+  // [
+  //   {
+  //     name: "Ross Vazquez",
+  //     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+  //     gender: "male"
+  //   },
+  //   {
+  //     name: "Sharlene Bush",
+  //     friends: ["Briana Decker", "Sharron Pace"],
+  //     gender: "female"
+  //   },
+  //   {
+  //     name: "Elma Head",
+  //     friends: ["Goldie Gentry", "Aisha Tran"],
+  //     gender: "female"
+  //   },
+  //   {
+  //     name: "Carey Barr",
+  //     friends: ["Jordan Sampson", "Eddie Strong"],
+  //     gender: "male"
+  //   },
+  //   {
+  //     name: "Blackburn Dotson",
+  //     friends: ["Jacklyn Lucas", "Linda Chapman"],
+  //     gender: "male"
+  //   },
+  //   {
+  //     name: "Sheree Anthony",
+  //     friends: ["Goldie Gentry", "Briana Decker"],
+  //     gender: "female"
+  //   },
+  //   {
+  //     name: "Moore Hensley",
+  //     friends: ["Sharron Pace"],
+  //     gender: "male"
+  //   }
+  // ]
+  
+
+console.log("");
+console.log("");
--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,57 @@
-function getShippingCost(country){
-    switch (country) {
-        case "China":
-            price = 100;
-           return `Shipping to ${country} will cost ${price} credits`;
-        case "Chile":
-            price = 250;
-           return `Shipping to ${country} will cost ${price} credits`;
-        case "Australia":
-            price = 170;
-           return `Shipping to ${country} will cost ${price} credits`;
-        case "Jamaica":
-            price = 120;
-           return `Shipping to ${country} will cost ${price} credits`;
-        default:
-           return `Sorry, there is no delivery to your country`;
-    }
+console.log("task-4");
+console.log("");
+
+getTotalBalanceByGender = (users, gender) => {
+    return users
+    .filter((user) => user.gender === gender)
+    .reduce((acc, total) => {
+        acc += total.balance
+        return acc
+    },0)
 }
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
+
+
+console.log("");
+console.log("");
